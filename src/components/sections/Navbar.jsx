93f1355359314a32641254e22ea685b912603575
Dineshv0311 <vdinesh31152k@gmail.com>
2025-12-28 18:51:03 +0530

    Updated the name

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar =({menuOpen,setMenuOpen})=>{
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className="font-mono text-xl font-bold text-white">
                     {""}
-                    <span className="text-blue-500">Dinesh</span>{""}
+                    <span className="text-blue-500">Dinesh V</span>{""}
                 </a>
                 <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={()=>setMenuOpen((prev)=>!prev)}>
                     &#9776;
